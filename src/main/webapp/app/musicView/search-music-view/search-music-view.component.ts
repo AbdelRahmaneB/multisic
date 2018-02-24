2348cb72b81f9a2430534891850cdb5753d2aab6
Francis <francis.rochon99@gmail.com>
2018-02-23 21:41:51 -0500

    setup component for search view

--- src/main/webapp/app/musicView/search-music-view/search-music-view.component.ts
+++ src/main/webapp/app/musicView/search-music-view/search-music-view.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'jhi-search-music-view',
-  templateUrl: './search-music-view.component.html',
-  styles: []
+    selector: 'jhi-search-music-view',
+    templateUrl: './search-music-view.component.html',
+    styleUrls: ['search-music-view.css'],
 })
 export class SearchMusicViewComponent implements OnInit {
+    napsterResults = ['test', 'test2'];
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {}
 }
