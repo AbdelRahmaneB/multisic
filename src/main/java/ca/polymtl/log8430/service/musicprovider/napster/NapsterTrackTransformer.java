2e340327e0879aa24f182b25eb405e14bd45cdce
pogc <pierre-olivier.guimond-cataford@polymtl.ca>
2018-03-07 12:26:01 -0500

    transformer napster

--- src/main/java/ca/polymtl/log8430/service/musicprovider/napster/NapsterTrackTransformer.java
+++ src/main/java/ca/polymtl/log8430/service/musicprovider/napster/NapsterTrackTransformer.java
@@ -1,18 +1,18 @@
 package ca.polymtl.log8430.service.musicprovider.napster;
 
 import ca.polymtl.log8430.domain.Track;
-import ca.polymtl.log8430.service.musicprovider.TrackTransformer;
+import org.json.simple.JSONObject;
 
-public class NapsterTrackTransformer implements TrackTransformer<com.zeloon.deezer.domain.Track> {
+public class NapsterTrackTransformer {
 
   @Override
-  public Track transform(com.zeloon.deezer.domain.Track fromTrack) {
+  public Track transform(JSONObject fromTrack) {
     Track toTrack = new Track();
-    toTrack.setName(fromTrack.getTitle());
-    toTrack.setArtist(fromTrack.getArtist().getName());
-    toTrack.setAlbum(fromTrack.getAlbum().getTitle());
-    toTrack.setPreviewurl(fromTrack.getPreview());
-    toTrack.setImagesurl(fromTrack.getAlbum().getCover());
+    toTrack.setName(fromTrack.get("name"));
+    toTrack.setArtist(fromTrack.get("artistName"));
+    toTrack.setAlbum(fromTrack.get("albumName"));
+    toTrack.setPreviewurl(fromTrack.get("previewURL")); 
+    toTrack.setImagesurl(fromTrack.get("name")); // get from api ie:http://direct.napster.com/imageserver/v2/albums/{album_id}/images/{size}.{extension}
     return toTrack;
   }
 
